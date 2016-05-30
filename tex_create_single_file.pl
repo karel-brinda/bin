3328e620ff3b866aa06429c93a407077ff267656
Karel Břinda <karel.brinda@gmail.com>
2016-05-30 19:43:24 +0200

    Script for manipulation with tex files

---
+++ tex_create_single_file.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl    
+
+# copied from http://tex.stackexchange.com/questions/62232/generating-a-single-tex-file-by-merging-different-tex-files
+
+### change the
+sub p_inc {
+  $DateiName = shift;
+    if ( open (my $datei, "$DateiName.tex") ) {
+      print "%%%---------- open: ", $DateiName, "\n";
+      while (<$datei>) {
+        if ((/^\s*\\include{\s+(\S+)/i) or (/^\s*\\input{\s+(\S+)/i)) {
+          my $include = $1;
+          chomp($include);chop($include);
+          print "%%%%%%%%% jump to ", $include, "\n";
+          p_inc($include);
+        } else { print unless /^\s*(#|$)/; }
+      }
+      print "%%%---------- close: ", $DateiName, "\n";
+      close $datei;
+    } else { print "%%%<===== file soesn't exist\n"; }
+}
+#
+@zeilen = (<>);
+for $zeile (@zeilen) {
+  next if $zeile =~ /^\s*(%)/;
+  if (($zeile =~ /^\s*\\include{\s*(\S+)/i) or ($zeile =~ /^\s*\\input{\s*(\S+)/i)) {      
+    my $include = $1;
+    chomp($include);
+    chop($include);
+    print "%%%%%%%%%%% jump to ", $include, "\n";
+    p_inc($include);
+  } else { print $zeile; }
+} 
