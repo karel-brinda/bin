b6a89af22232a8354d7ff739880d33d4ada8a2bc
Karel Brinda <karel.brinda@gmail.com>
2017-03-07 13:58:09 -0500

    Add arg parsing to the sample py file

--- samples/sample.py
+++ samples/sample.py
@@ -1,3 +1,18 @@
 #! /usr/bin/env python3
 
+import argparse
+import os
+import sys
 
+#if __name__ == "__main__":
+#	parser = argparse.ArgumentParser(description="")
+#
+#	parser.add_argument('-p', '--param',
+#			type=str,
+#			metavar='str',
+#			required=True,
+#			dest='',
+#			help='',
+#		)
+#
+#	args = parser.parse_args()
