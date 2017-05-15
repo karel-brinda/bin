0efdf17e6bb8b67e32f8b845e2f5da11e12c631a
Karel Břinda <kbrinda@hsph.harvard.edu>
2017-05-15 10:32:17 -0400

    Update sample.sh

--- samples/sample.sh
+++ samples/sample.sh
@@ -1,8 +1,25 @@
-#! /usr/bin/env bash -u -f -e -o pipefail
+#! /usr/bin/env bash
 
+set -u # requied variable initialization
+set -f # turn off wildcard expansion
+set -e # exit immediately if a command exits with a non-zero status
+set -o pipefail # fail if any command in a pipeline fails
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(readlink -m $(dirname $0))
+readonly ARGS="$@"
+
+usage() {
+    cat <<- EOF
+    usage: $PROGNAME options
+    EOF
+}
+
+main() {
 #if [ $# -ne 1 ]; then
-#	s=`basename $0`
-#	echo "usage: $s param"
+#	usage()
 #	exit 1
 #fi
+}
 
+main
