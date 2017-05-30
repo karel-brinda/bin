79f5f8d72051ada6fd8019221eace3874b494007
Karel Břinda <kbrinda@hsph.harvard.edu>
2017-05-30 15:45:33 -0400

    Update sample.sh

--- samples/sample.sh
+++ samples/sample.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-set -u # requied variable initialization
-set -f # turn off wildcard expansion
-set -e # exit immediately if a command exits with a non-zero status
-set -o pipefail # fail if any command in a pipeline fails
+set -u
+set -f
+set -e
+set -o pipefail
 
 readonly PROGNAME=$(basename $0)
 readonly PROGDIR=$(readlink -m $(dirname $0))
