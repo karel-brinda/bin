a54b77dedd25f1dd6bad8bd6ad19d336d2158ece
Karel Břinda <kbrinda@hsph.harvard.edu>
2017-11-07 13:30:31 -0500

    Add 'make help'

--- samples/sample.mk
+++ samples/sample.mk
@@ -1,4 +1,4 @@
-.PHONY: all clean
+.PHONY: all help clean
 
 SHELL=/usr/bin/env bash -euc -o pipefail
 
@@ -6,7 +6,9 @@ SHELL=/usr/bin/env bash -euc -o pipefail
 
 all:
 
+help: ## Print help message
+	    @echo "$$(grep -hE '^\S+:.*##' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' -e 's/^\(.\+\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' | column -c2 -t -s : | sort)"
 
-clean:
+clean: ## Clean
 
 
