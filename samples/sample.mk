ffb2a217e08899c80e41cdb361f2cb28c6622de7
Karel Břinda <karel.brinda@gmail.com>
2018-01-01 18:22:28 -0500

    Fix spaces

--- samples/sample.mk
+++ samples/sample.mk
@@ -7,7 +7,7 @@ SHELL=/usr/bin/env bash -eo pipefail
 all:
 
 help: ## Print help message
-	    @echo "$$(grep -hE '^\S+:.*##' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' -e 's/^\(.\+\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' | column -c2 -t -s : | sort)"
+	@echo "$$(grep -hE '^\S+:.*##' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' -e 's/^\(.\+\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' | column -c2 -t -s : | sort)"
 
 clean: ## Clean
 
