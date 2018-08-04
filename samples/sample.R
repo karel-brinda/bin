52ad090fce3cd298e2334bc1e8e1fff04f119f6e
Karel Břinda <karel.brinda@gmail.com>
2018-08-04 12:42:18 -0400

    Simply template for R

--- samples/sample.R
+++ samples/sample.R
@@ -3,55 +3,22 @@
 isRStudio <- Sys.getenv("RSTUDIO") == "1"
 set.seed(42)
 
-if (isRStudio){
-    srcFile = "table.tsv"
+if (isRStudio) {
+
 } else {
-
-    args <- commandArgs(trailingOnly = TRUE)
-    srcFile <- args[1]
-    outFile <- args[2]
-
-    pdf(outFile,
-        width=8,
-        height=8
-        )
+  pdf("plot.pdf",
+      width = 5,
+      height = 5)
 }
 
-df <- read.delim(srcFile, header = TRUE)
-
-x= plot(df)
+df <- read.delim("data.txt", header = TRUE)
 
 
-# title
-title(
-  main="t",
-  line=0.2
-)
 
-# subtitle
-title(
-  main=list("sub",cex=0.7),
-  line=-0.8,
-  cex=0.5
-)
 
-mtext(
-  "Y",
-  side=2,
-  line=1,
-  cex.lab=2,
-  las=3
-)
 
-mtext(
-  side=1,
-  text="Y",
-  line=1.8,
-  adj=0,
-  cex=0.80
-)
 
-if (! isRStudio){
-    dev.off()
+
+if (!isRStudio) {
+  dev.off()
 }
-
