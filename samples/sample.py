e34a0fac74580d7bc448ca342588c17de1851b8e
Karel Břinda <karel.brinda@gmail.com>
2018-10-27 18:31:40 -0400

    Yapf sample.py

--- samples/sample.py
+++ samples/sample.py
@@ -6,26 +6,31 @@ import os
 import re
 import sys
 
+
 def main():
     pass
 
 #    parser = argparse.ArgumentParser(description="")
 #
-#    parser.add_argument('file',
-#            type=argparse.FileType('r'),
-#            metavar='str',
-#            help='',
-#        )
+#    parser.add_argument(
+#        'file',
+#        type=argparse.FileType('r'),
+#        metavar='str',
+#        help='',
+#    )
 #
-#    parser.add_argument('-p', '--param',
-#            type=str,
-#            metavar='str',
-#            required=True,
-#            dest='',
-#            help='',
-#        )
-#
-#    args = parser.parse_args()
+#    parser.add_argument(
+#        '-p',
+#        '--param',
+#        type=str,
+#        metavar='str',
+#        required=True,
+#        dest='',
+#        help='',
+#    )
+
+    args = parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
