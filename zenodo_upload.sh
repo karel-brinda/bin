96e04e9b86afef3e4fa0e2c39b05d04bd01db45e
Karel Břinda <karel.brinda@gmail.com>
2021-10-15 16:01:33 -0400

    Update zenodo_upload.sh

--- zenodo_upload.sh
+++ zenodo_upload.sh
@@ -24,4 +24,4 @@ FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
 BUCKET=$(curl -H "Accept: application/json" -H "Authorization: Bearer $ZENODO_TOKEN" "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION" | jq --raw-output .links.bucket)
 
 
-curl --progress-bar -o /dev/null --upload-file $FILEPATH $BUCKET/$FILENAME?access_token=$ZENODO_TOKEN
+curl --progress-bar --upload-file $FILEPATH $BUCKET/$FILENAME?access_token=$ZENODO_TOKEN
