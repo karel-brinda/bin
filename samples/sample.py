dc0d29480e449789880dfefc19ddf1df09c2592c
Karel Břinda <karel.brinda@gmail.com>
2021-11-22 23:32:27 -0500

    Update sample Python file

--- samples/sample.py
+++ samples/sample.py
@@ -6,28 +6,29 @@ import os
 import re
 import sys
 
+from pathlib import Path
+from xopen import xopen
+from pprint import pprint
+
 
 def main():
-    pass
 
-    #    parser = argparse.ArgumentParser(description="")
-    #
-    #    parser.add_argument(
-    #        'file',
-    #        type=argparse.FileType('r'),
-    #        metavar='str',
-    #        help='',
-    #    )
-    #
-    #    parser.add_argument(
-    #        '-p',
-    #        '--param',
-    #        type=str,
-    #        metavar='str',
-    #        required=True,
-    #        dest='',
-    #        help='',
-    #    )
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument(
+        'txt_fn',
+        metavar='input.txt',
+        help='',
+    )
+
+    parser.add_argument(
+        '-p',
+        '--param',
+        metavar='str',
+        dest='param',
+        default='',
+        help='',
+    )
 
     args = parser.parse_args()
 
