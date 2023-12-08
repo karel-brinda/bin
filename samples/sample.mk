153f60245b0a64cbcf61b72d14af88bf651e6270
Karel Břinda <karel.brinda@gmail.com>
2023-12-08 20:44:02 +0100

    Fix help msg coloring

--- samples/sample.mk
+++ samples/sample.mk
@@ -8,8 +8,14 @@ SHELL=/usr/bin/env bash -eo pipefail
 
 all:
 
-help: ## Print help message
-	@echo "$$(grep -hE '^\S+:.*##' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' -e 's/^\(.\+\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' | column -c2 -t -s : | sort)"
+help: ## Print help messages
+	@echo -e "$$(grep -hE '^\S*(:.*)?##' $(MAKEFILE_LIST) \
+		| sed \
+			-e 's/:.*##\s*/:/' \
+			-e 's/^\(.*\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' \
+			-e 's/^\([^#]\)/\1/g' \
+			-e 's/^\(#.*#\)/\\x1b[31m\1\\x1b[m/' \
+		| column -c2 -t -s : )"
 
 clean: ## Clean
 
