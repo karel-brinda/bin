331ea2164de4a195bbf19d6c3673bfd4661ec50f
Karel Břinda <karel.brinda@gmail.com>
2023-12-14 15:15:40 +0100

    Update make help output format

--- samples/sample.mk
+++ samples/sample.mk
@@ -6,15 +6,20 @@ SHELL=/usr/bin/env bash -eo pipefail
 
 .SUFFIXES:
 
+##############
+## Commands ##
+##############
+
 all:
 
 help: ## Print help messages
 	@echo -e "$$(grep -hE '^\S*(:.*)?##' $(MAKEFILE_LIST) \
 		| sed \
 			-e 's/:.*##\s*/:/' \
-			-e 's/^\(.*\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' \
+			-e 's/^\(.*\):\(.*\)/   \\x1b[36m\1\\x1b[m:\2/' \
 			-e 's/^\([^#]\)/\1/g' \
-			-e 's/^\(#.*#\)/\\x1b[31m\1\\x1b[m/' \
+			-e 's/: /:/g' \
+			-e 's/^#\(.*\)#/\\x1b[90m\1\\x1b[m/' \
 		| column -c2 -t -s : )"
 
 clean: ## Clean
@@ -22,4 +27,3 @@ clean: ## Clean
 cleanall: clean ## Clean all
 
 view: ## View output
-	
