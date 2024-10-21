911184080d04c61700359fabe97f734c8bac8867
Karel Břinda <karel.brinda@gmail.com>
2024-10-21 18:42:52 +0200

    Add debugging line

--- samples/sample.sh
+++ samples/sample.sh
@@ -4,6 +4,7 @@ set -e
 set -o pipefail
 set -u
 #set -f
+#PS4='\[\e[32m\][$(date "+%Y-%m-%d %H:%M:%S") L${LINENO}]\[\e[0m\] '; set -x
 
 readonly PROGNAME=$(basename $0)
 readonly PROGDIR=$(dirname $0)
